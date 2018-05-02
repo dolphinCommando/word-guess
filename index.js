2921a8b3ff840ea41ba45d4ebad526118331d671
Matthew Buchanan <matt@matthews-air.home>
2018-05-02 19:11:25 -0400

    index.js takes commands and plays game

--- index.js
+++ index.js
@@ -2,5 +2,56 @@ var inquirer = require('inquirer');
 var Word = require('./word.js');
 const randomWord = require('random-word');
 
-console.log(randomWord());
-console.log(randomWord());
+const tries = 26;
+const word = randomWord();
+const wordObj = new Word(word);
+var guessed = [];
+var yourTries = 0;
+
+function play() {
+	console.log(wordObj.getWord().split('').join(' '));
+	if (tries <= 0) {
+		console.log('Sorry you lost! The word is ' + word);
+	}
+	else if (wordObj.getWord() === word) {
+		console.log('Congratulations you won!');
+		console.log(`It took you ${yourTries} tries.`);
+	}
+	else {
+		letterEnter();
+				
+	}
+}
+
+function letterEnter() {
+	inquirer.prompt([
+		{
+			name: 'char',
+			type: 'input',
+			message: `Guess a letter.`,
+		}
+	]).then(response => {
+		if (!validate(response.char)) {
+			console.log('Please enter a single letter');
+			return letterEnter();
+		}
+		else {
+			if (guessed.includes(response.char)) {
+				console.log('Letter already guessed.');
+			} 
+			else {
+				guessed.push(response.char);
+				var correct = wordObj.checkCorrect(response.char);
+				(correct) ? console.log('Correct!') : console.log('Nope.');
+			}
+			yourTries++;
+			play();
+		}
+	});
+}
+
+function validate(input) {
+	return (typeof input === 'string' && input.length===1);
+}
+
+play();
